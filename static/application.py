eccc07cb343d4fba8133f871c33b0aebd36aba12
yoanas_yonita <yonasyifter@gmail.com>
2019-03-29 20:56:34 +0900

    the first commit

--- static/application.py
+++ static/application.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
 
 @app.route("/more")
 def more():
     return render_template("more.html")
+@app.route("/index", methods=["GET","POST"])
+def index():
+    name=request.form.get('name')
+    age=request.form.get('age')
+    if age <21:
+        return render_template("index.html",name=name)
+    else:
+        return render_template('error.html')
 
 if __name__=="__main__":
     app.run(debug=True)
